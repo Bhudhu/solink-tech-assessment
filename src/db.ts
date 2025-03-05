4d5515d90daf1b0fa247bd075321d2126503f861
Tadiwa Mabhudhu <tadiwanashemabhudhu@ZCDCs-MacBook-Pro-2.local>
2025-03-05 10:35:56 +0200

    Prepare for Heroku deployment

--- src/db.ts
+++ src/db.ts
@@ -3,8 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set.");
+}
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Required for Heroku PostgreSQL
+  },
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
